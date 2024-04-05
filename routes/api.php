68078ed8c916e0efd8375972fea6d876d2ca6365
EveePagente <pagenteevelyn@gmail.com>
2024-04-06 00:03:35 +0800

    act-3

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +32,10 @@ Route::middleware('extract.token')->group(function(){
     Route::delete('/posts/{post}', [PostController::class, 'destroy']);
     
 });
+
+Route::apiResource('products', ProductController::class);
+// Route::get('/products', [ProductController::class, 'index']);
+// Route::post('/products', [ProductController::class, 'store']);
+// Route::get('/products/{product}', [ProductController::class, 'show']);
+// Route::put('/products/{product}', [ProductController::class, 'update']);
+// Route::delete('/products/{product}', [ProductController::class, 'destroy']);
